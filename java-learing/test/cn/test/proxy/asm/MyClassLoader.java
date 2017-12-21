8d2f434a9c70a1df9982a0fe7aa23d5aa0dfa006
yuyilun <773778632@qq.com>
2017-12-21 20:27:44 +0800

    add

---
+++ java-learing/test/cn/test/proxy/asm/MyClassLoader.java
@@ -0,0 +1,12 @@
+package cn.test.proxy.asm;
+
+public class MyClassLoader extends ClassLoader  {
+	
+	 public MyClassLoader() {  
+        super(Thread.currentThread().getContextClassLoader());  
+	 }  
+  
+	 public Class<?> defineClassForName(String name, byte[] data) {  
+        return this.defineClass(name, data, 0, data.length);  
+	 }  
+}
---
+++ java-learing/test/cn/test/proxy/asm/SecurityChecker.java
@@ -0,0 +1,8 @@
+package cn.test.proxy.asm;
+
+public class SecurityChecker {
+
+	 public static void checkSecurity() { 
+		 System.out.println("SecurityChecker.checkSecurity ..."); 
+	 } 	
+}
