7bffd4a994013898b7cf3a146bca4c2fdc10862b
yuyilun <773778632@qq.com>
2017-12-25 14:54:06 +0800

    learing asm

--- java-learing/test/cn/test/proxy/asm/MyClassLoader.java
+++ java-learing/test/cn/test/proxy/asm/MyClassLoader.java
@@ -9,4 +9,7 @@ public class MyClassLoader extends ClassLoader  {
 	 public Class<?> defineClassForName(String name, byte[] data) {  
         return this.defineClass(name, data, 0, data.length);  
 	 }  
+	 
+	 
+	 
 }
