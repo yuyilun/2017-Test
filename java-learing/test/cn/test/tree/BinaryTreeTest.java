a55a0892dd1c0e25e3711d3912d340c54f14b839
yuyilun <773778632@qq.com>
2018-01-03 14:10:32 +0800

    avl binary tree

--- java-learing/test/cn/test/tree/BinaryTreeTest.java
+++ java-learing/test/cn/test/tree/BinaryTreeTest.java
@@ -3,13 +3,15 @@ package cn.test.tree;
 import java.util.Random;
 import org.junit.Test;
 
+import cn.test.tree.AVLTree.AVLTreeNode;
+
 public class BinaryTreeTest {
 	
 	@Test
 	public void test() {
 		 //≤Â»Î  
 		BinaryTree bTree=new BinaryTree(56);
-		for(int i=0;i<10;i++){	
+		for(int i = 0 ; i < 10 ; i++ ){	
 			bTree.insert(new Random().nextInt(200));
 		}
 		
@@ -26,6 +28,26 @@ public class BinaryTreeTest {
 		
 		bTree.inorderTraversal();
 	} 
-
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void avlTest() {
+		int[] arr=new int[]{82,44,15,31,50,49,64,51,72,132,113,91,83,129,158,154,157,185,183,196};
+		AVLTree<Integer> avl=new AVLTree<>();
+		
+		AVLTreeNode tree = null;
+		for(int i = 0 ; i < 20 ; i++ ){	
+			tree=avl.insert(tree, arr[i]);
+		}
+		avl.preorderTraversalPub(tree);
+		avl.inorderTraversalPub(tree);
+		avl.postorderTraversalPub(tree);
+		
+		avl.remove(tree, 91);
+		
+		avl.preorderTraversalPub(tree);
+		avl.inorderTraversalPub(tree);
+		avl.postorderTraversalPub(tree);
+	}
 	
 }
