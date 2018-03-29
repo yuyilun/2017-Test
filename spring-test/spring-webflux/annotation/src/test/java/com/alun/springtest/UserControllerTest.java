31daed6a472c3df684318cbb262c9f86e8caba75
yuyilun <773778632@qq.com>
2018-03-29 16:15:47 +0800

    重新组合目录

---
+++ spring-test/spring-webflux/annotation/src/test/java/com/alun/springtest/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.alun.springtest;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.alun.springtest.domain.User;
+
+import reactor.core.publisher.Mono;
+
+public class UserControllerTest {
+
+	private final WebTestClient client = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
+	
+	@Test
+	public void testCreateUser() {
+		
+		final User user = new User();
+		user.setId("2");
+		user.setName("yuyilun");
+		user.setEmail("yu10081008@126.com");
+		
+		client.post().uri("/user")
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(Mono.just(user),User.class)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody().jsonPath("$[0].name").isEqualTo("yuyilun");
+	}
+
+}
